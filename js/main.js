4e56ffc27e3bb6bf068977893d98139f784a4789
German Leguizamon <96202044+germanl8@users.noreply.github.com>
2021-12-30 12:32:30 -0300

    formpeliculas

--- js/main.js
+++ js/main.js
@@ -4,13 +4,9 @@ window.addEventListener("load", function(){
 
     $(document).ready(function(){
     
-<<<<<<< HEAD
         $(document).ready(function(){
             $('.bxslider').bxSlider();
           });
-=======
-        
->>>>>>> 50cd0764448a804eb37120c20beda56a337cabb8
 
 
     });
