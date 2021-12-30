ed182b86d2924856de83687cca2a9269ff561c05
German Leguizamon <96202044+germanl8@users.noreply.github.com>
2021-12-29 23:21:10 -0300

    primero

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+window.addEventListener("load", function(){
+
+    $(document).ready(function(){
+    
+        $(document).ready(function(){
+            $('.bxslider').bxSlider();
+          });
+
+
+    });
+
+
+
+});
