869fbf3dbffacc86816e4358c65756b58c17ea0e
German Leguizamon <96202044+germanl8@users.noreply.github.com>
2022-01-03 15:07:55 -0300

    registro.js

--- js/registro.js
+++ js/registro.js
@@ -3,6 +3,41 @@
 
 window.addEventListener("load", function(){
 
+    
+    $("#form_reg").submit(function () { 
+        var mail = $("#input_mail").val();
+        var name = $("#input_name").val();
+        var surname = $("#input_surname").val();
+        var newpasswd = $("#input_newpasswd").val();
+        var repasswd = $("#input_repasswd").val();
+
+        if(newpasswd == repasswd){
+
+            localStorage.setItem("input_mail", mail);
+            localStorage.setItem("input_name", name);
+            localStorage.setItem("input_surname", surname);
+            localStorage.setItem("input_newpasswd", newpasswd);
+
+        }else alert("Las contraseñas no coinciden");
+
+      });
+    
+        var name = localStorage.getItem("name");
+        if(name != null && name != "undefined"){
+          var about_p = $("#about p");
+    
+          about_p.html("<strong>Bienvenido, "+name+"</strong>");
+          about_p.append("<a href='#' id='logout'> Cerrar Sesión</a>");
+    
+          $("#login").hide();
+          $("#about hr").show();
+          $("#logout").click(function () { 
+            localStorage.clear();
+            location.reload();
+            
+          });
+        }
+/*
 
     var form_reg_ = document.querySelector("#form_reg");
 
@@ -29,7 +64,7 @@ window.addEventListener("load", function(){
 
         }
 
-    });
+    }); */
 
 });
 
