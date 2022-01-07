66fc7717d52b1c3e74f7030f422c989e7a54764f
German Leguizamon <96202044+germanl8@users.noreply.github.com>
2022-01-07 09:27:45 -0300

    login.html - ingresar 0.1

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@
 
 window.addEventListener("load", function(){
 
-//----------link--registro-----------
+    //----------link--registro-----------
 
     var formregistrarme_ = document.querySelector("#formregistrarme");
 
@@ -17,8 +17,40 @@ window.addEventListener("load", function(){
 
 
 
-var user = document.querySelector("#input_user").value;
-var passwd = document.querySelector("#input_passwd").value;
+    var mail = $("#input_mail").val();
+    var passwd = $("#input_passwd").val();
+
+    var autentication = document.querySelector("#btnlogin");
+
+    autentication.addEventListener(`submit`, function(){
+
+        var alength = localStorage.length -1;
+        for (var i=alength; i>0; i){
+            var search_user = localStorage.getItem(i);
+            var string_user = JSON.parse(search_user);
+
+                if(string_user.mail == mail){
+                    var mail_existe = 1;
+                    console.log("dentro del if de mail");
+                    break;
+                    
+                }else mail_existe = 0;
+                i--;    
+        };
+
+        if(mail_existe != 0 ){ //!si mail existe en localstorage, chequea contraseña
+
+            if(string_user.newpasswd == passwd){
+                var passwd_ok = 1;
+                alert("inicio de sesion ok");
+                
+            }else passwd_ok = 0; alert("Contraseña equivocada");
+
+
+        }
+
+    });
+
 
 
 });
--- js/main.js
+++ js/main.js
@@ -2,7 +2,16 @@
 
 window.addEventListener("load", function(){
 
-    
+    var login = document.querySelector("#login");
+
+    login.addEventListener(`submit`, function(){
+
+        console.log("hola");
+
+            window.open("login.html", "Login", "width=550,height=400,resizable=NO,scrollbars=NO")
+            window.close();
+
+    });
 
 
 
